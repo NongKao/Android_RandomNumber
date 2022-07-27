70cdd8598eb3f4a60bfd9be06c671080206123be
longcao <nohope0201@gmail.com>
2022-07-27 14:18:16 +0700

    done

--- app/src/main/java/com/example/android_04/MainActivity.kt
+++ app/src/main/java/com/example/android_04/MainActivity.kt
@@ -1,35 +1,47 @@
 package com.example.android_04
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        Log.i("=========> " , "On create")
-
         setContentView(R.layout.activity_main)
+
+
+
+        btn_random.setOnClickListener(object : View.OnClickListener {
+            @SuppressLint("SetTextI18n")
+            override fun onClick(v: View?) {
+//                if (edt_From.text == "" || edt_To.text == "") return
+                val chuoi1 = edt_From.text.toString()
+                val chuoi2 = edt_To.text.toString()
+                if (chuoi1 == "" || chuoi2 == "") return
+                val so1 = chuoi1.toInt()
+                val so2: Int = chuoi2.toInt()
+                val random = Random
+
+                var so: Int = 0
+                if (so1 < so2) {
+                    so = random.nextInt(so1, so2)
+                } else {
+                    so = random.nextInt(so2, so1)
+                }
+                tv_random.setText(so.toString() + "")
+                println("==============>$so")
+
+            }
+
+        })
+
+
     }
 
-    override fun onStart() {
-        Log.i("=========> " , "On sstart")
-        super.onStart()
-    }
 
-    override fun onResume() {
-        Log.i("=========> " , "On resume")
-
-        super.onResume()
-    }
-
-        override fun onDestroy() {
-        Log.d("=========> " , "On destroy")
-        super.onDestroy()
-    }
-    override fun onStop() {
-        Log.i("=========> " , "On stop")
-        super.onStop()
-    }
 }
